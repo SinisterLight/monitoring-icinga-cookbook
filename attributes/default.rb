2501f5808da896e0ec3e1dde61208cab1a4c20bc
Sinister Light <me@sinisterlight.com>
2015-04-20 19:30:09 +0530

    [Shobhit] installing latest version

--- attributes/default.rb
+++ attributes/default.rb
@@ -10,6 +10,7 @@ default[:icinga2][:ido][:load_schema] = false
 default[:icinga2][:ido][:db_host] = '127.0.0.1'
 default[:icinga2][:disable_conf_d] = true
 default[:mysql][:server_root_password] = "Ch4ng3me"
+default[:icinga2][:ignore_version] = true
 
 default['monitoring-icinga'][:user] = "admin"
 default['monitoring-icinga'][:password] = "admin"
